60dbc33530a21e055e9260a579709e95d5bc7c98
Dorian Goepp <dorian.goepp@inria.fr>
2015-08-06 16:36:15 +0200

    improve readibility of error messages from dynamixel

--- src/dynamixel/status.cpp
+++ src/dynamixel/status.cpp
@@ -37,15 +37,15 @@ namespace dynamixel
             std::string error_str = boost::lexical_cast<std::string>(_id);
             // we could have many errors in the same packet ?
             if (error & 1) // bit 0
-                error_str += "Input voltage error;";
+                error_str += " Input voltage error;";
             if (error & 2) // bit 1
-                error_str += "Angle limit error;";
+                error_str += " Angle limit error;";
             if (error & 4) // bit 2
-                error_str += "Overheating error;";
+                error_str += " Overheating error;";
             if (error & 8) // bit 3
-                error_str += "Range error;";
+                error_str += " Range error;";
             if (error & 16) // bit 4
-                error_str += "Checksum error;";
+                error_str += " Checksum error;";
             if (error & 32) // bit 5
             {
                 error_str += "Overload error;";
