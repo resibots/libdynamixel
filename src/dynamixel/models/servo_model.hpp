77b8a45772edf7cfd0722dd131de48f5df6c3879
Federico Allocati <fede.allocati@gmail.com>
2016-01-26 19:34:00 +0100

    Finished API for all models of version 1

--- src/dynamixel/models/servo_model.hpp
+++ src/dynamixel/models/servo_model.hpp
@@ -14,11 +14,55 @@
 #include "../instructions/sync_write.hpp"
 #include "model_traits.hpp"
 
+#define BASE_READ_FIELD(Name) \
+    static InstructionPacket<protocol_t> get_##Name(typename protocol_t::id_t id)   \
+    {                                                                                                                                     \
+        return read_t(id, ct_t::Name, sizeof(typename ct_t::Name##_t));                           \
+    }
+
+#define BASE_WRITE_FIELD(Name) \
+    static InstructionPacket<protocol_t> set_##Name(typename protocol_t::id_t id, typename ct_t::Name##_t value, bool reg = false)  \
+    {                                                                                                                                                                                                                           \
+            if (reg)                                                                                                                                                                                                          \
+                return reg_write_t(id, ct_t::Name, protocol_t::pack_data(value));                                                                                                \
+            else                                                                                                                                                                                                               \
+                return write_t(id, ct_t::Name, protocol_t::pack_data(value));                                                                                                       \
+    }
+
+#define BASE_READ_WRITE_FIELD(Name) BASE_READ_FIELD(Name) \
+    BASE_WRITE_FIELD(Name)
+
+#define READ_FIELD(Name) \
+    static InstructionPacket<typename ServoModel<Model>::protocol_t> get_##Name(typename ServoModel<Model>::protocol_t::id_t id)   \
+    {                                                                                                                                     \
+        return typename ServoModel<Model>::read_t(id, ServoModel<Model>::ct_t::Name, sizeof(typename ServoModel<Model>::ct_t::Name##_t));                           \
+    }
+
+#define WRITE_FIELD(Name) \
+    static InstructionPacket<typename ServoModel<Model>::protocol_t> set_##Name(typename ServoModel<Model>::protocol_t::id_t id, typename ServoModel<Model>::ct_t::Name##_t value, bool reg = false)  \
+    {                                                                                                                                                                                                                           \
+            if (reg)                                                                                                                                                                                                          \
+                return typename ServoModel<Model>::reg_write_t(id, ServoModel<Model>::ct_t::Name, ServoModel<Model>::protocol_t::pack_data(value));                                                                                                \
+            else                                                                                                                                                                                                               \
+                return typename ServoModel<Model>::write_t(id, ServoModel<Model>::ct_t::Name, ServoModel<Model>::protocol_t::pack_data(value));                                                                                                       \
+    }
+
+#define WRITE_BOOL_FIELD(Name, Bool_Name) \
+    static InstructionPacket<typename ServoModel<Model>::protocol_t> set_##Name(typename ServoModel<Model>::protocol_t::id_t id, bool value, bool reg = false)  \
+    {                                                                                                                                                                                                                           \
+            if (reg)                                                                                                                                                                                                          \
+                return typename ServoModel<Model>::reg_write_t(id, ServoModel<Model>::ct_t::Name, ServoModel<Model>::protocol_t::pack_data(value ? ServoModel<Model>::ct_t::Bool_Name##_on : ServoModel<Model>::ct_t::Bool_Name##_off)); \
+            else                                                                                                                                                                                                               \
+                return typename ServoModel<Model>::write_t(id, ServoModel<Model>::ct_t::Name, ServoModel<Model>::protocol_t::pack_data(value ? ServoModel<Model>::ct_t::Bool_Name##_on : ServoModel<Model>::ct_t::Bool_Name##_off));                                                                                                       \
+    }
+
+#define READ_WRITE_FIELD(Name) READ_FIELD(Name) \
+    WRITE_FIELD(Name)
+
 namespace dynamixel {
 namespace models {
     template <class Model>
-    class ServoModel {
-    public:
+    struct ServoModel {
         typedef ModelTraits<Model> traits_t;
         typedef typename traits_t::protocol_t protocol_t;
         typedef typename traits_t::CT ct_t;
@@ -30,98 +74,50 @@ namespace models {
         typedef instructions::FactoryReset<protocol_t> factory_reset_t;
         typedef instructions::SyncWrite<protocol_t> sync_write_t;
 
-        static InstructionPacket<protocol_t> set_id(typename protocol_t::id_t old_id, typename protocol_t::id_t new_id, bool reg = false)
-        {
-            if (reg)
-                return reg_write_t(old_id, ct_t::id, protocol_t::pack_data(new_id));
-            else
-                return write_t(old_id, ct_t::id, protocol_t::pack_data(new_id));
-        }
-
-        static InstructionPacket<protocol_t> set_baudrate(typename protocol_t::id_t id, typename ct_t::baudrate_t baudrate, bool reg = false)
-        {
-            if (reg)
-                return reg_write_t(id, ct_t::baudrate, protocol_t::pack_data(baudrate));
-            else
-                return write_t(id, ct_t::baudrate, protocol_t::pack_data(baudrate));
-        }
-
-        static InstructionPacket<protocol_t> set_torque_enabled(typename protocol_t::id_t id, bool enabled, bool reg = false)
-        {
-            if (reg)
-                return reg_write_t(id, ct_t::torque_enable, protocol_t::pack_data((typename ct_t::torque_enable_t)(enabled ? 1 : 0)));
-            else
-                return write_t(id, ct_t::torque_enable, protocol_t::pack_data((typename ct_t::torque_enable_t)(enabled ? 1 : 0)));
-        }
-
-        static InstructionPacket<protocol_t> set_led(typename protocol_t::id_t id, bool enabled, bool reg = false)
-        {
-            if (reg)
-                return reg_write_t(id, ct_t::led, protocol_t::pack_data(enabled ? ct_t::led_on : ct_t::led_off));
-            else
-                return write_t(id, ct_t::led, protocol_t::pack_data(enabled ? ct_t::led_on : ct_t::led_off));
-        }
-
-        static InstructionPacket<protocol_t> set_position(typename protocol_t::id_t id, typename ct_t::goal_pos_t pos, bool reg = false)
-        {
-            if (reg)
-                return reg_write_t(id, ct_t::goal_pos, protocol_t::pack_data(pos));
-            else
-                return write_t(id, ct_t::goal_pos, protocol_t::pack_data(pos));
-        }
+        BASE_READ_FIELD(model_number);
+        BASE_READ_FIELD(firmware_version);
+        BASE_READ_WRITE_FIELD(id);
+        BASE_READ_WRITE_FIELD(baudrate);
+        BASE_READ_WRITE_FIELD(return_delay_time);
+        BASE_READ_WRITE_FIELD(highest_temperature_limit);
+        BASE_READ_WRITE_FIELD(highest_voltage_limit);
+        BASE_READ_WRITE_FIELD(lowest_voltage_limit);
+        BASE_READ_WRITE_FIELD(status_return_level);
+        BASE_READ_WRITE_FIELD(alarm_shutdown);
+        BASE_READ_WRITE_FIELD(torque_enable);
+        BASE_READ_WRITE_FIELD(goal_position);
+        BASE_READ_WRITE_FIELD(moving_speed);
+        BASE_READ_FIELD(present_position);
+        BASE_READ_FIELD(present_speed);
+        BASE_READ_FIELD(present_voltage);
+        BASE_READ_FIELD(present_temperature);
+        BASE_READ_FIELD(registered);
+        BASE_READ_FIELD(moving);
 
         template <typename Id, typename Pos>
-        static InstructionPacket<protocol_t> set_positions(const std::vector<Id>& ids, const std::vector<Pos>& pos)
+        static InstructionPacket<protocol_t> set_goal_positions(const std::vector<Id>& ids, const std::vector<Pos>& pos)
         {
             assert(ids.size() == pos.size());
             std::vector<std::vector<uint8_t> > packed(pos.size());
             for (size_t i = 0; i < pos.size(); i++)
-                packed[i] = protocol_t::pack_data((typename ct_t::goal_pos_t)pos[i]);
+                packed[i] = protocol_t::pack_data((typename ct_t::goal_position_t)pos[i]);
 
-            return sync_write_t(ct_t::goal_pos, _get_typed<typename protocol_t::id_t>(ids), packed);
-        }
-
-        static InstructionPacket<protocol_t> set_speed(typename protocol_t::id_t id, typename ct_t::moving_speed_t speed, bool reg = false)
-        {
-            if (reg)
-                return reg_write_t(id, ct_t::moving_speed, protocol_t::pack_data(speed));
-            else
-                return write_t(id, ct_t::moving_speed, protocol_t::pack_data(speed));
+            return sync_write_t(ct_t::goal_position, get_typed<typename protocol_t::id_t>(ids), packed);
         }
 
         template <typename Id, typename Speed>
-        static InstructionPacket<protocol_t> set_speeds(const std::vector<Id>& ids, const std::vector<Speed>& speeds)
+        static InstructionPacket<protocol_t> set_moving_speeds(const std::vector<Id>& ids, const std::vector<Speed>& speeds)
         {
             assert(ids.size() == speeds.size());
             std::vector<std::vector<uint8_t> > packed(speeds.size());
             for (size_t i = 0; i < speeds.size(); i++)
                 packed[i] = protocol_t::pack_data((typename ct_t::moving_speed_t)speeds[i]);
 
-            return sync_write_t(ct_t::moving_speed, _get_typed<typename protocol_t::id_t>(ids), packed);
+            return sync_write_t(ct_t::moving_speed, get_typed<typename protocol_t::id_t>(ids), packed);
         }
 
-        static InstructionPacket<protocol_t> set_torque_limit(typename protocol_t::id_t id, typename ct_t::torque_limit_t torque_limit, bool reg = false)
-        {
-            if (reg)
-                return reg_write_t(id, ct_t::torque_limit, protocol_t::pack_data(torque_limit));
-            else
-                return write_t(id, ct_t::torque_limit, protocol_t::pack_data(torque_limit));
-        }
-
-        template <typename Id, typename TorqueLimit>
-        static InstructionPacket<protocol_t> set_torque_limits(const std::vector<Id>& ids, const std::vector<TorqueLimit>& torque_limits)
-        {
-            assert(ids.size() == torque_limits.size());
-            std::vector<std::vector<uint8_t> > packed(torque_limits.size());
-            for (size_t i = 0; i < torque_limits.size(); i++)
-                packed[i] = protocol_t::pack_data((typename ct_t::torque_limit_t)torque_limits[i]);
-
-            return sync_write_t(ct_t::torque_limit, _get_typed<typename protocol_t::id_t>(ids), packed);
-        }
-
-    protected:
         template <typename T, typename U>
-        static std::vector<T> _get_typed(std::vector<U> vector)
+        static std::vector<T> get_typed(std::vector<U> vector)
         {
             std::vector<T> result(vector.size());
             for (size_t i = 0; i < vector.size(); i++)
@@ -129,6 +125,133 @@ namespace models {
 
             return result;
         }
+    };
+
+    template <class Model>
+    struct AngleLimitServoModel {
+        READ_WRITE_FIELD(cw_angle_limit);
+        READ_WRITE_FIELD(ccw_angle_limit);
+    };
+
+    template <class Model>
+    struct DriveModeServoModel {
+        READ_WRITE_FIELD(drive_mode);
+    };
+
+    template <class Model>
+    struct MaxTorqueServoModel {
+        READ_WRITE_FIELD(max_torque);
+    };
+
+    template <class Model>
+    struct AlarmLedServoModel {
+        READ_WRITE_FIELD(alarm_led);
+    };
+
+    template <class Model>
+    struct MultiTurnOffsetServoModel {
+        READ_WRITE_FIELD(multi_turn_offset);
+    };
+
+    template <class Model>
+    struct ResolutionDividerServoModel {
+        READ_WRITE_FIELD(resolution_divider);
+    };
+
+    template <class Model>
+    struct SingleLedServoModel {
+        READ_FIELD(led);
+        WRITE_BOOL_FIELD(led, led);
+    };
+
+    template <class Model>
+    struct RGBLedServoModel {
+        READ_FIELD(led_r);
+        READ_FIELD(led_g);
+        READ_FIELD(led_b);
+        WRITE_BOOL_FIELD(led_r, led);
+        WRITE_BOOL_FIELD(led_g, led);
+        WRITE_BOOL_FIELD(led_b, led);
+    };
+
+    template <class Model>
+    struct DynRGBLedServoModel {
+        READ_WRITE_FIELD(led_r);
+        READ_WRITE_FIELD(led_g);
+        READ_WRITE_FIELD(led_b);
+    };
+
+    template <class Model>
+    struct ComplianceServoModel {
+        READ_WRITE_FIELD(cw_compliance_margin);
+        READ_WRITE_FIELD(ccw_compliance_margin);
+        READ_WRITE_FIELD(cw_compliance_slope);
+        READ_WRITE_FIELD(ccw_compliance_slope);
+    };
+
+    template <class Model>
+    struct PidServoModel {
+        READ_WRITE_FIELD(d_gain);
+        READ_WRITE_FIELD(i_gain);
+        READ_WRITE_FIELD(p_gain);
+    };
+
+    template <class Model>
+    struct TorqueLimitServoModel {
+        READ_WRITE_FIELD(torque_limit);
+
+        template <typename Id, typename TorqueLimit>
+        static InstructionPacket<typename ServoModel<Model>::protocol_t> set_torque_limits(const std::vector<Id>& ids, const std::vector<TorqueLimit>& torque_limits)
+        {
+            assert(ids.size() == torque_limits.size());
+            std::vector<std::vector<uint8_t> > packed(torque_limits.size());
+            for (size_t i = 0; i < torque_limits.size(); i++)
+                packed[i] = ServoModel<Model>::protocol_t::pack_data((typename ServoModel<Model>::ct_t::torque_limit_t)torque_limits[i]);
+
+            return typename ServoModel<Model>::sync_write_t(ServoModel<Model>::ct_t::torque_limit, ServoModel<Model>::template get_typed<typename ServoModel<Model>::protocol_t::id_t>(ids), packed);
+        }
+    };
+
+    template <class Model>
+    struct PresentLoadServoModel {
+        READ_FIELD(present_load);
+    };
+
+    template <class Model>
+    struct LockServoModel {
+        READ_FIELD(lock);
+        WRITE_BOOL_FIELD(lock, lock);
+    };
+
+    template <class Model>
+    struct PunchServoModel {
+        READ_WRITE_FIELD(punch);
+    };
+
+    template <class Model>
+    struct SensedCurrentServoModel {
+        READ_FIELD(sensed_current);
+    };
+
+    template <class Model>
+    struct CurrentServoModel {
+        READ_FIELD(current);
+    };
+
+    template <class Model>
+    struct TorqueControlModeServoModel {
+        READ_FIELD(torque_control_mode_enabled);
+        WRITE_BOOL_FIELD(torque_control_mode_enabled, torque_control_mode_enabled);
+    };
+
+    template <class Model>
+    struct GoalTorqueServoModel {
+        READ_WRITE_FIELD(goal_torque);
+    };
+
+    template <class Model>
+    struct GoalAccelerationServoModel {
+        READ_WRITE_FIELD(goal_acceleration);
     };
 }
 }
