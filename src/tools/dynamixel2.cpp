9c7c1290638ec3c0f4ee53e5e7932d76b4d1182c
Dorian Goepp <dorian.goepp@inria.fr>
2016-04-06 16:30:59 +0200

    Add two commands and start a third one.
    The commands list and position are implemented and tested on AX-12+ and
    MX-64 (no device with protocol 2). Implementation for get-position is
    ongoing.

--- src/tools/dynamixel2.cpp
+++ src/tools/dynamixel2.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include <algorithm>
 #include <dynamixel/dynamixel.hpp>
 #include <cmath>
@@ -7,44 +8,118 @@
 #include "tools/utility.hpp"
 
 using namespace dynamixel;
+namespace po = boost::program_options;
 
-void list(Utility&)
-{
-}
+// TODO: catch exceptions !
 
-void select_command(const std::string& command, Utility& tool)
-{
-    if ("list" == command)
-        list(tool);
+namespace dynamixel {
+    template <class Protocol>
+    class CommandLineUtility {
+    public:
+        CommandLineUtility(const std::string& name, int baudrate = B115200, double recv_timeout = 0.1) : _dyn_util(name, baudrate, recv_timeout)
+        {
+        }
+
+        void select_command(po::variables_map vm)
+        {
+            std::string command = vm["command"].as<std::string>();
+
+            if ("list" == command) {
+                list();
+            }
+            else if ("position" == command) {
+                position(vm["id"].as<std::vector<long long int>>(),
+                    vm["angle"].as<std::vector<double>>());
+            }
+            else if ("get-position" == command) {
+                get_position(vm["id"].as<std::vector<long long int>>());
+            }
+        }
+
+    private:
+        Utility<Protocol> _dyn_util;
+
+        void list()
+        {
+            _dyn_util.detect_servos();
+            std::vector<std::shared_ptr<BaseServo<Protocol>>> actuators = _dyn_util.list();
+
+            std::cout << "Connected devices (" << actuators.size() << ") :" << std::endl;
+            for (auto actuator : actuators) {
+                std::cout << actuator->id() << " : " << actuator->model_name() << std::endl;
+            }
+        }
+
+        void position(std::vector<long long int> ids, std::vector<double> angles)
+        {
+            if (angles.size() == 1) {
+                _dyn_util.detect_servos();
+                _dyn_util.set_angle(ids, angles.at(0));
+            }
+            else if (ids.size() == angles.size()) {
+                _dyn_util.detect_servos();
+                _dyn_util.set_angle(ids, angles);
+            }
+            else
+                std::cout << "Usage for position command: " << std::endl
+                          << "- one angle and several ids of servos that all "
+                             "wil go to the same angle"
+                          << std::endl
+                          << "- as many angles as there are ids, to give target "
+                             "angle for each servo"
+                          << std::endl;
+            // TODO: manage limit cases
+        }
+
+        void get_position(std::vector<long long int> ids)
+        {
+            std::vector<double> positions;
+
+            if (ids.size() > 0)
+                positions = _dyn_util.get_angles(ids);
+            else
+                positions = _dyn_util.get_angles();
+
+            std::cout << "Angular positions of the actuators:" << std::endl;
+            for (unsigned i = 0; i < ids.size(); ++i) {
+                std::cout << ids[i] << "\t" << positions[i] << std::endl;
+            }
+        }
+    };
 }
 
 //void parse_input TODO: put input parsing in a dedicated function
 
+// TODO: show help message for each command if called with `--help command`
+
 int main(int argc, char** argv)
 {
     std::string port;
-    int baudrate;
+    int baudrate = 0, posix_baudrate = 0;
     float timeout;
     std::string command;
-
-    namespace po = boost::program_options;
 
     po::options_description desc("Allowed options");
     // clang-format off
     desc.add_options()
         ("help,h", "produce help message")
         ("port,p", po::value<std::string>()->default_value("/dev/ttyUSB0"),
-            "path to the USB to dynamixel interface.\n\n"
+            "path to the USB to dynamixel interface.\n"
             "Example: --port /dev/ttyACM5")
         ("baudrate,b", po::value<unsigned>()->default_value(1000000),
-            "baud rate for the communication\n\n"
+            "baud rate for the communication\n"
             "Example: -b 115200")
         ("timeout,t", po::value<float>(&timeout)->default_value(0.02),
             "timeout for the reception of data packets")
+        ("id", po::value<std::vector<long long int>>()->multitoken(),
+            "one or more ids of devices")
+        ("angle", po::value<std::vector<double>>()->multitoken(),
+            "desired angle, used for the \"position\" command")
         ("command", po::value<std::string>(), "command to be executed. "
             "Available commands are:\n"
-            "\tlist\t list available actuators"
-            "\tangle\t command one or more actuator to go to (a) given position(s)");
+            "  list     list available actuators"
+            "  position command one or more actuator to go to (a) given position(s)"
+            "  get-position retrieve current angular position of one or more servo");
     // clang-format on
 
     po::positional_options_description pos_desc;
@@ -64,21 +139,30 @@ int main(int argc, char** argv)
     }
     if (vm.count("command"))
         command = vm["command"].as<std::string>();
+    else {
+        std::cerr << "No command given to the software!" << std::endl;
+        std::cout << desc << std::endl;
+        return 1;
+    }
     if (vm.count("port"))
         port = vm["port"].as<std::string>();
-    if (vm.count("baudrate"))
-        baudrate = get_baudrate(vm["baudrate"].as<unsigned>());
+    if (vm.count("baudrate")) {
+        baudrate = vm["baudrate"].as<unsigned>();
+        posix_baudrate = get_baudrate(baudrate);
+    }
 
     try {
+        using Protocol = protocols::Protocol1;
+
         std::cout << "Opening serial interface" << std::endl
                   << "\tport: " << port << std::endl
                   << "\tbaudrate: " << baudrate << std::endl;
-        Utility tool(port, baudrate, timeout);
+        CommandLineUtility<Protocol> command_line(port, posix_baudrate, timeout);
         std::cout << "Serial port open." << std::endl;
 
         std::cout << "\nExecuting the following command" << std::endl
                   << "\tcommand: " << command << std::endl;
-        select_command(command, tool);
+        command_line.select_command(vm);
     }
     catch (errors::Error e) {
         std::cerr << "Error (dynamixel): " << e.msg() << std::endl;
--- src/tools/utility.hpp
+++ src/tools/utility.hpp
@@ -3,6 +3,10 @@
 
 #include <dynamixel/dynamixel.hpp>
 
+#include <map>
+#include <sstream>
+#include <stdexcept>
+
 using namespace dynamixel;
 using namespace controllers;
 using namespace servos;
@@ -10,21 +14,151 @@ using namespace instructions;
 using namespace protocols;
 
 namespace dynamixel {
+    template <class Protocol>
     class Utility {
     public:
-        Utility(const std::string& name, int baudrate = B115200, double recv_timeout = 0.1) : _serial_interface(name, baudrate, recv_timeout)
+        Utility(const std::string& name, int baudrate = B115200, double recv_timeout = 0.1) : _serial_interface(name, baudrate, recv_timeout), _scanned(false)
         {
-            _servos = auto_detect<Protocol1>(_serial_interface);
         }
 
-        std::vector<std::shared_ptr<BaseServo<Protocol1>>>& list()
+        void detect_servos(double scan_timeout = 0.01)
         {
+            double original_timeout = _serial_interface.recv_timeout();
+            _serial_interface.set_recv_timeout(scan_timeout);
+
+            _servos = auto_detect<Protocol>(_serial_interface);
+            _scanned = true;
+
+            _serial_interface.set_recv_timeout(original_timeout);
+
+            // Build a map from the actuator id to the index in _servos
+            typename std::vector<std::shared_ptr<BaseServo<Protocol>>>::iterator servo_it;
+            for (servo_it = _servos.begin(); servo_it != _servos.end(); ++servo_it) {
+                _ids_map[(*servo_it)->id()] = servo_it - _servos.begin();
+            }
+        }
+
+        /** Return the list of connected actuators.
+            If we didn't do a scanning yet, does it with the default recieve timout.
+
+            @return vector of (std) shared pointers to BaseServo instances
+        **/
+        const std::vector<std::shared_ptr<BaseServo<Protocol>>>& list()
+        {
+            if (!_scanned)
+                detect_servos();
+
             return _servos;
+        }
+
+        const std::map<long long int, unsigned>& ids_map() const
+        {
+            if (!_scanned)
+                throw std::runtime_error("ids_map(): you need to scan for connected actuators before asking for the map of IDs");
+
+            return _ids_map;
+        }
+
+        const std::map<long long int, unsigned>& ids_map()
+        {
+            return _ids_map;
+        }
+
+        void set_angle(long long int id, double angle)
+        {
+            // FIXME: check that the id indeed matches one of the detected servos
+            // FIXME: fix the exception thrown if we ask an angle beyond limits. Currently, the message speaks about ticks whereas we are talking in rad.
+            _serial_interface.send(_servos[id]->set_goal_position_angle(angle));
+        }
+
+        void set_angle(std::vector<long long int> ids, double angle)
+        {
+            for (auto id : ids) {
+                std::shared_ptr<BaseServo<Protocol>> servo = _servos[_ids_map[id]];
+                _serial_interface.send(servo->reg_goal_position_angle(angle));
+
+                StatusPacket<Protocol> status;
+                _serial_interface.recv(status);
+            }
+
+            _serial_interface.send(dynamixel::instructions::Action<Protocol>(Protocol::broadcast_id));
+        }
+
+        void set_angle(std::vector<long long int> ids, std::vector<double> angles)
+        {
+            if (ids.size() != angles.size())
+                throw std::runtime_error("set_position(vector, vector): the "
+                                         "vectors of IDs and angles should have "
+                                         "the same length");
+
+            for (int i = 0; i < ids.size(); i++) {
+                std::shared_ptr<BaseServo<Protocol>> servo = _servos[_ids_map[ids[i]]];
+                _serial_interface.send(servo->reg_goal_position_angle(angles[i]));
+
+                StatusPacket<Protocol> status;
+                _serial_interface.recv(status);
+            }
+
+            _serial_interface.send(dynamixel::instructions::Action<Protocol>(Protocol::broadcast_id));
+        }
+
+        std::vector<double> get_angles(std::vector<long long int> ids)
+        {
+            std::vector<double> positions(ids.size());
+
+            for (unsigned i = 0; i < ids.size(); ++i) {
+                StatusPacket<Protocol> status;
+                // request current position
+                _serial_interface.send(
+                    _servos[_ids_map[ids[i]]]->get_present_position_angle());
+                _serial_interface.recv(status);
+
+                // parse response to get the position
+                if (status.valid())
+                    positions[i] = _servos[_ids_map[ids[i]]]
+                                       ->parse_present_position_angle(status);
+                else {
+                    std::stringstream message;
+                    message << "Did not receive any data when reading "
+                            << ids[i] << "'s position";
+                    throw std::runtime_error(message.str());
+                }
+            }
+
+            return positions;
+        }
+
+        std::vector<double> get_angles()
+        {
+            std::vector<double> positions(_servos.size());
+
+            for (unsigned i = 0; i < _servos.size(); ++i) {
+                StatusPacket<Protocol> status;
+                // request current position
+                _serial_interface.send(
+                    _servos[i]->get_present_position_angle());
+                _serial_interface.recv(status);
+
+                // parse response to get the position
+                if (status.valid())
+                    positions[i] = _servos[i]
+                                       ->parse_present_position_angle(status);
+                else {
+                    std::stringstream message;
+                    message << "Did not receive any data when reading "
+                            << _servos[i]->id() << "'s position";
+                    throw std::runtime_error(message.str());
+                }
+            }
+
+            return positions;
         }
 
     private:
         Usb2Dynamixel _serial_interface;
-        std::vector<std::shared_ptr<BaseServo<Protocol1>>> _servos;
+        std::vector<std::shared_ptr<BaseServo<Protocol>>> _servos;
+        std::map<long long int, unsigned> _ids_map;
+        bool _scanned;
     };
 }
 
