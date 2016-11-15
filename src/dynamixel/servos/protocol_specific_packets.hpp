resibots/libdynamixel

commit fe5fc46bef688d309d7efcea89565715d94ce6df

fix language-related typo in file name

src/dynamixel/servos/protocol_specific_packets.hpp
@@ -1,5 +1,5 @@
-#ifndef DYNAMIXEL_SERVOS_PROTOSPECIFICPACQUETS_HPP_
-#define DYNAMIXEL_SERVOS_PROTOSPECIFICPACQUETS_HPP_
+#define DYNAMIXEL_SERVOS_PROTOSPECIFICPACKETS_HPP_
+#ifndef DYNAMIXEL_SERVOS_PROTOSPECIFICPACKETS_HPP_
 
 #include <cmath>
 
@@ -159,4 +159,4 @@ namespace dynamixel {
     }
 }
 
-#endif
+#endif // DYNAMIXEL_SERVOS_PROTOSPECIFICPACKETS_HPP_

commit 8d907972c5167915c0013e05ecb0554d8f028ab4

Fix error introduced by fe5fc46bef
I made the huge mistake of not compiling after making changes...

src/dynamixel/servos/protocol_specific_packets.hpp
@@ -1,5 +1,5 @@
-#define DYNAMIXEL_SERVOS_PROTOSPECIFICPACKETS_HPP_
 #ifndef DYNAMIXEL_SERVOS_PROTOSPECIFICPACKETS_HPP_
+#define DYNAMIXEL_SERVOS_PROTOSPECIFICPACKETS_HPP_
 
 #include <cmath>
 

src/dynamixel/servos/servo.hpp
@@ -7,7 +7,7 @@
 #include "../instruction_packet.hpp"
 #include "../status_packet.hpp"
 #include "base_servo.hpp"
-#include "protocol_specific_pacquets.hpp"
+#include "protocol_specific_packets.hpp"
 #include "../instructions/ping.hpp"
 #include "../instructions/read.hpp"
 #include "../instructions/write.hpp"