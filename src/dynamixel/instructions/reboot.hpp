48ac92bff7c45c022d19ab20f9ae597b584a7764
Dorian Goepp <dorian.goepp@inria.fr>
2017-02-07 15:40:12 +0100

    add instruction for reboot (protocol 2 only)

---
+++ src/dynamixel/instructions/reboot.hpp
@@ -0,0 +1,18 @@
+#ifndef DYNAMIXEL_INSTRUCTIONS_REBOOT_HPP_
+#define DYNAMIXEL_INSTRUCTIONS_REBOOT_HPP_
+// #include <typetraits>
+
+#include "../instruction_packet.hpp"
+
+namespace dynamixel {
+    namespace instructions {
+        template <class T>
+        class Reboot : public InstructionPacket<T> {
+        public:
+            Reboot(typename T::id_t id)
+                : InstructionPacket<T>(id, T::Instructions::reboot) {}
+        };
+    }
+}
+
+#endif
