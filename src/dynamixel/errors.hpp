08da6ca7b12d4b3fa356f0eb9148e1a23935cdd9
Dorian Goepp <dorian.goepp@inria.fr>
2017-03-03 13:40:08 +0100

    new exception to report issues with packet contents

--- src/dynamixel/errors.hpp
+++ src/dynamixel/errors.hpp
@@ -2,6 +2,7 @@
 #define DYNAMIXEL_ERRORS_HPP_
 
 #include "errors/error.hpp"
+#include "errors/bad_packet.hpp"
 #include "errors/crc_error.hpp"
 #include "errors/servo_limit_error.hpp"
 #include "errors/status_error.hpp"
