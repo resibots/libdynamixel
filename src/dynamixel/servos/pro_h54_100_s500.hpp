940473a66db638401bb01983c4b7cf660bfa482c
Dorian Goepp <dorian.goepp@inria.fr>
2017-03-06 14:40:42 +0100

    also fix the velocity control gains

--- src/dynamixel/servos/pro_h54_100_s500.hpp
+++ src/dynamixel/servos/pro_h54_100_s500.hpp
@@ -60,9 +60,9 @@ namespace dynamixel {
                 typedef uint8_t led_g_t;
                 static const protocol_t::address_t led_b = 565;
                 typedef uint8_t led_b_t;
-                static const protocol_t::address_t velocity_i_gain = 26;
+                static const protocol_t::address_t velocity_i_gain = 586;
                 typedef uint16_t velocity_i_gain_t;
-                static const protocol_t::address_t velocity_p_gain = 27;
+                static const protocol_t::address_t velocity_p_gain = 588;
                 typedef uint16_t velocity_p_gain_t;
                 static const protocol_t::address_t position_p_gain = 594;
                 typedef uint16_t position_p_gain_t;
